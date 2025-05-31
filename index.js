1a3e5e3d64fa142f30e3f1f3cebe64cd2d5b2019
William-Schroeder1406 <william.schroeder.1224@gmail.com>
2025-05-31 10:24:14 -0300

    aula-2

---
+++ index.js
@@ -0,0 +1,12 @@
+const nome = "William";
+let idade = "18";
+let cidade = "Piratuba";
+
+console.log (nome);
+console.log(idade);
+console.log(cidade);
+
+console.log(nome + " tem " + idade + " anos e sua cidade é " + cidade)
+
+let maioridade = idade >= 18;
+console.log("é maior de idade", maioridade);
---
+++ verificacoes.js
@@ -0,0 +1,4 @@
+let nota = 10;
+let media = nota >= 7;
+console.log("sua nota é " + nota + " " + media);
+
